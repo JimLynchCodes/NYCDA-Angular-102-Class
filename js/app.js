3b3c6b196de74d1fa67e1005c1d4a5989a1bb0cb
James Lynch <jim@fantasyup.com>
2016-02-17 00:24:55 -0500

    no message

--- js/app.js
+++ js/app.js
@@ -2,56 +2,74 @@ angular.module('MyApp', [])
 
     .controller('MainController', function($scope) {
         var self = this;
-        // self.greeting = 'Hello World!';
 
-        console.log("FUCK");
-        // self.simpleArray = ['Item1', 'Item2', 'Item3'];
+        self.folderOrItemRadioButton = "folder";
 
-        // self.interArray = [{
-        // 	name: 'george'
-        // }, {
-        // 	name: 'Harry'
-        // }, {
-        // 	name: 'Sally'
-        // }];
-
-
-        // self.states = {
-        // 	CA:'Clifornia',
-        // 	NY: 'New York',
-        // 	FL: 'Florida'
-        // };
-
-        // self.username = 'tommy';
-
-        self.daFuck = function() {
-          console.log("ok.. " + self.classObject.red);
-        };
-
-        self.myDog = {
-            name:'Max'
-        };
-
-        self.myCat = {
-            name:'Sally'
-        };
-
-        self.myCow = {
-            name:'Harry'
-        };
-
-        self.animalSelected = self.myCow;
-
-
-        self.onButtonClick = function () {
-            alert('you clicked the button!');
-        };
-
-        self.classObject = {
+        self.allFalseObject = {
             red:false,
+            blue:false,
             green:false,
             yellow:false
+        };
+
+        self.deselectNoneCheckbox = function() {
+            self.isNoneSelected = false;
         }
+
+        self.classObject = self.allFalseObject;
+
+        self.noneCheckBoxClicked = function() {
+
+            console.log("none clicked");
+            self.classObject = { red:false,
+                blue:false,
+                green:false,
+                yellow:false}
+        };
+
+        self.submitButtonClicked = function() {
+            console.log("the input text is " + self.currentInputText);
+
+            var newItem = {
+                name: self.currentInputText,
+                items: []
+            };
+
+            self.folders.push(newItem);
+
+            console.log(self);
+            console.log(this);
+
+        };
+
+        self.folders = [
+            {
+                name:"first folder",
+                items: [
+                    {
+                        name: "First item"
+                    },
+                    {
+                        name: "Seconds Item"
+                    }
+
+                ]
+            },
+            {
+                name:"second folder",
+                items: [
+                    {
+                        name: "First item"
+                    },
+                    {
+                        name: "Seconds Item"
+                    }
+
+                ]
+            }
+
+        ];
+
 
     });
 
